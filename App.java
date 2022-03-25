df86074f1eead3f85ebe91ca8bdc6abeef5cd91f
Prof. Daniel Callegari <daniel.callegari@pucrs.br>
2022-03-25 18:53:05 -0300

    meus amigos

--- App.java
+++ App.java
@@ -2,7 +2,9 @@ public class App
 {
   public static void main (String[] args)
   {
-    System.out.println("Ola, turma de GCS!");  
+    System.out.println("Ola, turma de GCS!");
+	
+    System.out.println("Fim do programa.");
     
   }
   
